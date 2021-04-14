5bfabd02f430200aa17338d93a2240a8fd13454b
Reza Ghifary <fachrezaghifary@gmail.com>
2021-04-14 13:34:32 +0700

    add client

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,15 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
+import request from "supertest";
 import mongoose from "mongoose";
-// import { app } from "../app";
+import { app } from "../app";
+
+declare global {
+    namespace NodeJS {
+        interface Global {
+            signin(): Promise<string[]>;
+        }
+    }
+}
 
 //before start
 let mongo: any;
@@ -27,3 +36,17 @@ afterAll(async () => {
     await mongo.stop();
     await mongoose.connection.close();
 });
+
+// global fn that only available at test env
+global.signin = async () => {
+    const email = "test@test.com";
+    const password = "password";
+
+    const signupResponse = await request(app)
+        .post("/api/users/signup")
+        .send({ email, password })
+        .expect(201);
+    const cookie = signupResponse.get("Set-Cookie");
+
+    return cookie;
+};
