0b1f038c9e6d450dad67624bb7f0fa2927b9495f
Reza Ghifary <goacgras@gmail.com>
2021-04-19 12:01:58 +0700

    change MONGO_URI in auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,8 +5,11 @@ const start = async () => {
     if (!process.env.JWT_KEY) {
         throw new Error("JWT_KEY must be define");
     }
+    if (!process.env.MONGO_URI) {
+        throw new Error("MONGO_URI must be define");
+    }
     try {
-        await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
+        await mongoose.connect(process.env.MONGO_URI, {
             useUnifiedTopology: true,
             useNewUrlParser: true,
             useCreateIndex: true,
