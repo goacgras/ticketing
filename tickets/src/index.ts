84069c3c56bc874751fa127fa836ca1a1ffdf86f
Reza Ghifary <goacgras@gmail.com>
2021-04-24 23:39:59 +0700

    add env variables in ticket

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -11,8 +11,22 @@ const start = async () => {
         throw new Error("MONGO_URI must be defined");
     }
     try {
+        if (!process.env.NATS_CLIENT_ID) {
+            throw new Error("NATS_CLIENT_ID must be defined");
+        }
+        if (!process.env.NATS_URL) {
+            throw new Error("NATS_URL must be defined");
+        }
+
+        if (!process.env.NATS_CLUSTER_ID) {
+            throw new Error("NATS_CLUSTER_ID must be defined");
+        }
         //clusterId created at depl file
-        await natsWrapper.connect("ticketing", "abc", "http://nats-srv:4222");
+        await natsWrapper.connect(
+            process.env.NATS_CLUSTER_ID,
+            process.env.NATS_CLIENT_ID,
+            process.env.NATS_URL
+        );
 
         // Proper closing NATS server
         natsWrapper.client.on("close", () => {
