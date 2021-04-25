709bf42659e48e63a4bf8a4446d182760fdbc7e0
Reza Ghifary <goacgras@gmail.com>
2021-04-25 15:56:19 +0700

    add order routes

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -6,10 +6,10 @@ import {
     errorHandler,
     currentUser,
 } from "@grasticketing/common";
-import { createTicketRouter } from "./routes/new";
-import { showTicketRouter } from "./routes/show";
-import { indexTicketRouter } from "./routes";
-import { updataTicketRouter } from "./routes/update";
+import { newOrderRouter } from "./routes/new";
+import { showOrderRouter } from "./routes/show";
+import { indexOrderRouter } from "./routes";
+import { deleteOrderRouter } from "./routes/delete";
 
 const app = express();
 //trust proxy behind ingress nginx
@@ -26,10 +26,10 @@ app.use(
 //middleware
 app.use(currentUser);
 
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updataTicketRouter);
+app.use(indexOrderRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
+app.use(deleteOrderRouter);
 
 app.all("*", async () => {
     throw new NotFoundError();
