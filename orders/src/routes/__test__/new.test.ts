e2e2402d7a06d4eca0e86d982355479a9c18500b
Reza Ghifary <goacgras@gmail.com>
2021-04-25 22:56:42 +0700

    create order automated test

---
+++ orders/src/routes/__test__/new.test.ts
@@ -0,0 +1,60 @@
+import request from "supertest";
+import { app } from "../../app";
+import mongoose from "mongoose";
+import { Order, OrderStatus } from "../../models/order";
+import { Ticket } from "../../models/ticket";
+
+it("returns error if ticket does not exist", async () => {
+    const ticketId = mongoose.Types.ObjectId();
+
+    await request(app)
+        .post("/api/orders")
+        .set("Cookie", global.signin())
+        .send({
+            ticketId,
+        })
+        .expect(404);
+});
+it("returns error if ticket already reserved", async () => {
+    //create ticket
+    const ticket = Ticket.build({
+        title: "new ticket",
+        price: 100,
+    });
+    await ticket.save();
+
+    //create order to ticket above
+    const order = Order.build({
+        ticket,
+        userId: "123123",
+        status: OrderStatus.Created,
+        expiresAt: new Date(),
+    });
+    await order.save();
+
+    //make a request to order reserved ticket
+    await request(app)
+        .post("/api/orders")
+        .set("Cookie", global.signin())
+        .send({
+            ticketId: ticket.id,
+        })
+        .expect(400);
+});
+it("reserve a ticket", async () => {
+    //create ticket
+    const ticket = Ticket.build({
+        title: "new ticket",
+        price: 100,
+    });
+    await ticket.save();
+
+    //order a ticket
+    await request(app)
+        .post("/api/orders")
+        .set("Cookie", global.signin())
+        .send({
+            ticketId: ticket.id,
+        })
+        .expect(201);
+});
