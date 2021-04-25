30ffdda42d571cb84dffd64d7411adea5c726967
Reza Ghifary <goacgras@gmail.com>
2021-04-25 22:10:11 +0700

    add isReserved method to ticket doc

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,7 +1,15 @@
 import mongoose from "mongoose";
-import { requireAuth, validateRequest } from "@grasticketing/common";
+import {
+    BadRequestError,
+    NotFoundError,
+    OrderStatus,
+    requireAuth,
+    validateRequest,
+} from "@grasticketing/common";
 import { Request, Response, Router } from "express";
 import { body } from "express-validator";
+import { Ticket } from "../models/ticket";
+import { Order } from "../models/order";
 
 const router = Router();
 
@@ -17,6 +25,39 @@ router.post(
     ],
     validateRequest,
     async (req: Request, res: Response) => {
+        const { ticketId } = req.body;
+
+        //find the ticket that user is trying to order
+        const ticket = await Ticket.findById(ticketId);
+        if (!ticket) {
+            throw new NotFoundError();
+        }
+
+        //Check the ticket status, make sure it is not reserved
+        //Run query to look all orders. Find an order where the ticket,
+        //is the ticket we just found *and* the order status is *not* cancelled
+        //if we find an order from that means the ticket *is* reserved
+        //meaning: find order with status below, if we found, user cant attempt to reserved
+        const existingOrder = await Order.findOne({
+            ticket: ticket,
+            status: {
+                $in: [
+                    OrderStatus.Created,
+                    OrderStatus.Complete,
+                    OrderStatus.AwaitingPayment,
+                ],
+            },
+        });
+        if (existingOrder) {
+            throw new BadRequestError("Ticket is already reserved");
+        }
+
+        //calculate the expiration
+
+        //build the order and save it to db
+
+        //publish an event
+
         res.send({});
     }
 );
