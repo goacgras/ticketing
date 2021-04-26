31749f07a761c81edf49d006edc8e3bce29dd776
Reza Ghifary <goacgras@gmail.com>
2021-04-26 13:50:38 +0700

    add Order/index route and the tests

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,9 +1,15 @@
+import { requireAuth } from "@grasticketing/common";
 import { Request, Response, Router } from "express";
+import { Order } from "../models/order";
 
 const router = Router();
 
-router.get("/api/orders", async (req: Request, res: Response) => {
-    res.send({});
+router.get("/api/orders", requireAuth, async (req: Request, res: Response) => {
+    const orders = await Order.find({
+        userId: req.currentUser!.id,
+    }).populate("ticket"); //column name;
+
+    res.send(orders);
 });
 
 export { router as indexOrderRouter };
