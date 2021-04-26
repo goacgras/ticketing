363bf5b77e8352f65b4f0e8b1fe71f0ec38e3e1c
Reza Ghifary <goacgras@gmail.com>
2021-04-26 14:50:09 +0700

    create order delete router

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,28 @@
+import {
+    NotAuthorizedError,
+    NotFoundError,
+    OrderStatus,
+} from "@grasticketing/common";
 import { Request, Response, Router } from "express";
+import { Order } from "../models/order";
 
 const router = Router();
 
+//Cancelling the order
 router.delete("/api/orders/:orderId", async (req: Request, res: Response) => {
-    res.send({});
+    const { orderId } = req.params;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) throw new NotFoundError();
+    if (order.userId !== req.currentUser!.id) {
+        throw new NotAuthorizedError();
+    }
+
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    res.status(204).send(order);
 });
 
 export { router as deleteOrderRouter };
