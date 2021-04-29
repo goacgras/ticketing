86626bb3938714b08d03daeba404015c5f224266
Reza Ghifary <goacgras@gmail.com>
2021-04-29 21:28:21 +0700

    add publisher to queue

--- expiration/src/queues/expiration-queue.ts
+++ expiration/src/queues/expiration-queue.ts
@@ -1,4 +1,6 @@
 import Queue from "bull";
+import { ExpirationCompletePublisher } from "../events/publishers/expiration-complete-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 // create a generic interface
 interface Payload {
@@ -14,10 +16,9 @@ const expirationQueue = new Queue<Payload>("order:expiration", {
 
 // what to do after finish the job
 expirationQueue.process(async (job) => {
-    console.log(
-        "I want to publish an expiration:complete event for orderId",
-        job.data.orderId
-    );
+    new ExpirationCompletePublisher(natsWrapper.client).publish({
+        orderId: job.data.orderId,
+    });
 });
 
 export { expirationQueue };
