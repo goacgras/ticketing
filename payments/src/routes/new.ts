f738859f4ace99d9e28c57434217eae8f5379a7d
Reza Ghifary <goacgras@gmail.com>
2021-04-30 21:08:39 +0700

    add cases logic

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -1,6 +1,14 @@
-import { requireAuth, validateRequest } from "@grasticketing/common";
+import {
+    BadRequestError,
+    NotAuthorizedError,
+    NotFoundError,
+    OrderStatus,
+    requireAuth,
+    validateRequest,
+} from "@grasticketing/common";
 import { Request, Response, Router } from "express";
 import { body } from "express-validator";
+import { Order } from "../models/order";
 
 const router = Router();
 
@@ -15,7 +23,25 @@ router.post(
             .withMessage("orderId must not be empty"),
     ],
     validateRequest,
-    (req: Request, res: Response) => {
+    async (req: Request, res: Response) => {
+        const { token, orderId } = req.body;
+
+        // find the order
+        const order = await Order.findById(orderId);
+
+        // check if exist
+        if (!order) throw new NotFoundError();
+
+        // check if user create the order
+        if (order.userId !== req.currentUser!.id) {
+            throw new NotAuthorizedError();
+        }
+
+        // check if order is cancelled
+        if (order.status === OrderStatus.Cancelled) {
+            throw new BadRequestError("Cannot pay for an cancelled order");
+        }
+
         res.send({ success: true });
     }
 );
