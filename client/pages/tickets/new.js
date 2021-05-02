8f8e507e298fd187af15ef546d85e61d6e76da28
Reza Ghifary <goacgras@gmail.com>
2021-05-02 21:41:44 +0700

    create form and sanitize

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -1,15 +1,42 @@
+import { useState } from "react";
+
 const NewTicket = () => {
+    const [title, setTitle] = useState("");
+    const [price, setPrice] = useState("");
+
+    const onBlur = () => {
+        const value = parseFloat(price);
+
+        isNaN(value) ? null : setPrice(value.toFixed(2));
+
+        // if(isNaN(value)){
+        //     return
+        // }
+
+        // setPrice(value.toFixed(2))
+    };
+
     return (
         <div>
             <h1>Create Ticket</h1>
             <form>
                 <div className='form-group'>
                     <label>Title</label>
-                    <input className='form-control' />
+                    <input
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        className='form-control'
+                    />
                 </div>
                 <div className='form-group'>
                     <label>Price</label>
-                    <input className='form-control' />
+                    <input
+                        value={price}
+                        // when user click out
+                        onBlur={onBlur}
+                        onChange={(e) => setPrice(e.target.value)}
+                        className='form-control'
+                    />
                 </div>
                 <button className='btn btn-primary'>Submit</button>
             </form>
