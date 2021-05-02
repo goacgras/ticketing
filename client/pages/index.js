ae00482390653ebc49cd3ac045d176876f477893
Reza Ghifary <goacgras@gmail.com>
2021-05-02 23:46:40 +0700

    create tickets/orderId route

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,10 +1,16 @@
-// import buildClient from "../api/build-client";
+import Link from "next/link";
+
 // we're passing currentUser property from _app and passes down to child component
 const Home = ({ currentUser, tickets }) => {
     const ticketList = tickets.map((ticket) => (
         <tr key={ticket.id}>
             <td>{ticket.title}</td>
             <td>${ticket.price}</td>
+            <td>
+                <Link href={`/tickets/${ticket.id}`}>
+                    <a>View</a>
+                </Link>
+            </td>
         </tr>
     ));
 
@@ -16,6 +22,7 @@ const Home = ({ currentUser, tickets }) => {
                     <tr>
                         <th>Title</th>
                         <th>Price</th>
+                        <th>Link</th>
                     </tr>
                 </thead>
                 <tbody>{ticketList}</tbody>
